3439f9e85f599808edb2954937a78db6b5ad2107
EuiJin Kim <justwavz@gmail.com>
2023-03-04 16:56:40 +0900

    [#17] 🔧 local font 세팅 - pretendard font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-pretendard)', ...fontFamily.sans],
+      },
       colors: {
         // primary
         'primary-50': '#EAE8FF',
