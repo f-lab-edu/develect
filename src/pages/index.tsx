00124f38f77a964f704677abbfa5e6281cde4d2e
EuiJin Kim <justwavz@gmail.com>
2023-03-06 21:40:02 +0900

    [#18] ✨ 기본 그리드 값 설정 및 레이아웃 테스트 화면 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@ import { BasicLayout } from '@/components/layouts/basic-layout'
 import { NextPageWithLayout } from './_app'
 
 const Home: NextPageWithLayout = () => (
-  <BasicLayout>
-    <p className="heading-m font-bold">Hello World</p>
-  </BasicLayout>
+  <div className="basic-grid">
+    {Array.from({ length: 12 }, (_, i) => i).map((key) => (
+      <div key={key} className="h-screen w-full bg-red-50" />
+    ))}
+  </div>
 )
 
 Home.getLayout = function getLayout(page: ReactElement) {
