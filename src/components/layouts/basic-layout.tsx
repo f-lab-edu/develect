e72da97eed7fc9fb20eb0ad46cfb70c52f5f2e58
EuiJin Kim <justwavz@gmail.com>
2023-03-06 21:22:54 +0900

    [#18] ✨ layout container 값 설정

--- src/components/layouts/basic-layout.tsx
+++ src/components/layouts/basic-layout.tsx
@@ -5,9 +5,11 @@ interface BasicLayoutProps {
 }
 
 export const BasicLayout = ({ children }: BasicLayoutProps) => (
-  <div className={`${pretendard.className} font-sans`}>
+  <div
+    className={`${pretendard.className} flex min-h-screen flex-col font-sans`}
+  >
     <header />
-    <main>{children}</main>
+    <main className="container flex-auto">{children}</main>
     <footer />
   </div>
 )
