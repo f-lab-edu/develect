3bbd01e9380e3dabf869085c0f0583412459f9e3
EuiJin Kim <justwavz@gmail.com>
2023-03-06 23:37:18 +0900

    [#18] 🔧 eslint import/order rule 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import type { ReactElement } from 'react'
 import { BasicLayout } from '@/components/layouts/basic-layout'
+
 import { NextPageWithLayout } from './_app'
 
+import type { ReactElement } from 'react'
+
 const Home: NextPageWithLayout = () => (
+  // TODO: 테스트용 임시 코드(제거 예정)
   <div className="basic-grid">
     {Array.from({ length: 12 }, (_, i) => i).map((key) => (
       <div key={key} className="h-screen w-full bg-red-50" />
