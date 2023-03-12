8abd535646d89d074c1d386520db4869848a895c
euijin <justwavz@gmail.com>
2023-03-12 23:50:57 +0900

    [#20] 🔐 Google Client, DB 환경변수 추가

--- process-env.d.ts
+++ process-env.d.ts
@@ -1,6 +1,13 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test'
+    DB_USERNAME: string
+    DB_PASSWORD: string
+    DB_HOST: string
+    DB_PORT: string
+    DB_NAME: string
     DATABASE_URL: string
+    GOOGLE_CLIENT_ID: string
+    GOOGLE_CLIENT_SECRET: string
   }
 }
