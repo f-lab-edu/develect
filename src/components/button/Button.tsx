3fb64e67c4523bfeb44cc14c696ee1b0000419f6
euijin <justwavz@gmail.com>
2023-03-13 21:40:26 +0900

    [#20] ♻️ Header 로그인 시 로그아웃 버튼 표시

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,10 +1,11 @@
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode
 }
 
 // TODO: Button 컴포넌트 구현
-export const Button = ({ children }: ButtonProps) => (
+export const Button = ({ children, ...buttonProps }: ButtonProps) => (
   <button
+    {...buttonProps}
     type="button"
     className="rounded-[32px] bg-primary-400 px-8 py-2.5 transition hover:bg-primary-500"
   >
